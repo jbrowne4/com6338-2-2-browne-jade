abc8ec095c31e02a9f43a4b32a6bb2c37c12c333
Jade Browne <jade.browne@outlook.com>
2024-08-28 20:49:35 -0400

    name and age

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code here
+function greet() {
+    let name = prompt ("What is your name?");
+    console.log("Hello, " + name);
+    alert("Hello, Sally");
+    
+    let age = parseInt(prompt ("How old are you?"));
+    console.log(age);
+
+    let hasHadBirthday = parseInt(prompt ("Have you had a birthday yet this year?"));
+    console.log(hasHadBirthday);
+
+    var bucket = 0
+    
+    if (hasHadBirthday) {
+        console.log(birthYear = currentYear - age);
+    } else {
+        console.log(birthYear = currentYear - age - 1);
+    }
+
+    let currentyear = new Date().getFullYear();
+     
+
+    alert("You were born in" + birthYear);
+}
